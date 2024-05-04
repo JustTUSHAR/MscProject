768ac105ebc580c1c2eeeede74acb86162496289
JustTUSHAR <theswaruptushar@gmail.com>
2024-05-04 10:06:52 +0530

    Adding W^- Raa plot

--- python_work/plot_lab.py
+++ python_work/plot_lab.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 my_xticks = ['0%-20%','20%-40%','40%-80%']
 #x=[1,2,3,4]
 x=[1,2,3]
-y=[0.3198,0.0332,0.0446]
+y=[0.5081,0.1636,0.1901]
+#y=[0.3198,0.0332,0.0446]
 #y=[1.99,0.202,1.529,0.041]
 #y=[0.066,0.103,0.235,0.070]
 #x=[10,20,30,40,80,100]
@@ -20,7 +21,7 @@ plt.plot(x,y,'g^')
 font1 = {'family':'serif','color':'blue','size':20}
 plt.xlabel("Centraility",fontdict = font1)
 plt.xticks(x, my_xticks)
-plt.ylabel("RAA ",fontdict = font1)
+plt.ylabel("RAA for W+",fontdict = font1)
 plt.ylim(0, 2)
 plt.axhline(y=1, color='r', linestyle='-',marker='D')
 plt.legend(['Raa points','Reference line'])
